feeec3cbb9e05bb82026dc3cef516ae7371a9232
william blackie <william.blackie@torchbox.com>
2020-02-07 21:25:54 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'django_storybook', 
-  packages = ['django_storybook'],   
-  version = '0.1',      
+  version = '0.1.1',      
   license='MIT',        
-  description = 'A Django module integrating Storybook pattern library and Django',   # Give a short description about your library
+  description = 'A Django module integrating Storybook pattern library and Django',
+      long_description=long_description,
+    long_description_content_type='text/markdown',
   author = 'William Blackie',                   
   author_email = 'will@williamblackie.com',      
   url = 'https://github.com/William-Blackie/django_storybook',  
   download_url = 'https://github.com/William-Blackie/django_storybook/archive/v_01.tar.gz',  
   keywords = ['Django', 'Pattern-library', 'Storybook', 'Pattern library'],  
-  install_requires=[
-            'django-cors-headers==3.2.1',
-            'Django>=2.2,<2.3'
-      ],
+  packages=setuptools.find_packages(),
   classifiers=[
     'Development Status :: 3 - Alpha',   
     'Intended Audience :: Developers', 
@@ -21,4 +23,5 @@ setup(
     'License :: OSI Approved :: MIT License', 
     'Programming Language :: Python :: 3.6',
   ],
+  python_requires='>=3.6',
 )
